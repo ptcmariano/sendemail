d8014113d509c98befe66f61a266ee496fb6b2c0
unknown <paulo.mariano@PC_PAULOM.mega.com.br>
2012-12-12 13:17:30 -0200

    Classes of send email

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MgSendEmail
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            SendEmail se = new SendEmail();
+            if (args.Length == 7)
+            {
+                se.MgSendEmail(args[0], args[1], args[0], args[2], args[3], args[4], args[5], Int32.Parse(args[6]));
+                return 0;
+            }
+
+            if (args.Length == 3)
+            {
+                se.MgSendEmail("user@domain.com.br", "password", "user@domain.com.br", args[0], args[1], args[2], "smtp.gmail.com", 25);
+                return 0;
+            }
+
+            Console.WriteLine("Informe 7 ou 3 argumentos:");
+            Console.WriteLine("emitente senha destinatario assunto mensagem servidor porta");
+            Console.WriteLine("OU");
+            Console.WriteLine("destinatarios assunto mensagem");
+            
+            return 1;
+        }
+    }
+}
---
+++ SendEmail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace MgSendEmail
+{
+    class SendEmail
+    {
+        public void MgSendEmail(string user, string pass,
+								string from, string to, 
+								string subject, string message, 
+								string server, int port)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Host = server;
+            client.Port = port;
+            client.EnableSsl = true;
+            System.Net.NetworkCredential nc = new System.Net.NetworkCredential(user, pass);
+            client.Credentials = nc;
+            try
+            {
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress(from);
+                msg.To.Add(to);
+                msg.Subject = subject;
+                msg.Body = message;
+                client.Send(msg);
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine("Erro de formato dos endereços: " + f);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao enviar email: "+e);
+            }
+            
+        }
+    }
+}
